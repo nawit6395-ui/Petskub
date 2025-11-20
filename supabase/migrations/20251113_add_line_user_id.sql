32518178613d435116507b87612319b21915f623
nawit <nawit6395@gmail.com>
2025-11-20 23:22:49 +0700

    CookiePolicy

--- supabase/migrations/20251113_add_line_user_id.sql
+++
@@ -1,3 +0,0 @@
--- Add line_user_id column to profiles table for LINE authentication
-ALTER TABLE profiles ADD COLUMN line_user_id TEXT UNIQUE;
-CREATE INDEX idx_profiles_line_user_id ON profiles(line_user_id);
