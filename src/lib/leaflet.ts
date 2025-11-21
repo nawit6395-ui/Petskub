2062c55e638166a332de2706e1576f7887395cc8
nawit <nawit6395@gmail.com>
2025-11-21 16:26:47 +0700

    update1

--- src/lib/leaflet.ts
+++ src/lib/leaflet.ts
@@ -1,9 +1,10 @@
 import L from "leaflet";
-import locationMarker from "@/assets/location.png";
 
 const iconRetinaUrl = new URL("leaflet/dist/images/marker-icon-2x.png", import.meta.url).toString();
 const iconUrl = new URL("leaflet/dist/images/marker-icon.png", import.meta.url).toString();
 const shadowUrl = new URL("leaflet/dist/images/marker-shadow.png", import.meta.url).toString();
+const redPinSvg = `<svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg"><path d="M32 2C20.4 2 11 11.4 11 23c0 14.3 12.7 27.7 19.4 34.5a2 2 0 0 0 2.8 0C40.9 50.7 53 38.4 53 23 53 11.4 43.6 2 32 2Z" fill="#f43f5e"/><circle cx="32" cy="24" r="9" fill="white"/></svg>`;
+const redPinDataUrl = `data:image/svg+xml;utf8,${encodeURIComponent(redPinSvg)}`;
 
 export const defaultMarkerIcon = L.icon({
   iconRetinaUrl,
@@ -16,9 +17,9 @@ export const defaultMarkerIcon = L.icon({
 });
 
 export const locationMarkerIcon = L.icon({
-  iconUrl: locationMarker,
-  iconSize: [48, 48],
-  iconAnchor: [24, 44],
+  iconUrl: redPinDataUrl,
+  iconSize: [44, 44],
+  iconAnchor: [22, 40],
   popupAnchor: [0, -34],
 });
 
